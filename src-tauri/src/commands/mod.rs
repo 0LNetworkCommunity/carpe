cdd31e68e360b95889e2bdcb42cf4598db0befe6
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-10-22 11:03:40 -0400

    move swarm commands out of mod

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -18,10 +18,8 @@ use tower::block::{
 };
 
 use tower::commit_proof::commit_proof_tx;
-use ol::commands::init_cmd::initialize_host_swarm;
 use ol_types::config::{self, TxType};
 use serde::{Deserialize, Serialize};
-use sysinfo::{ProcessExt, SystemExt};
 use txs::submit_tx::{TxParams, eval_tx_status, get_tx_params_from_swarm, tx_params};
 
 
@@ -69,72 +67,6 @@ pub fn keygen() -> Result<String, String> {
     Ok(t) => Ok(t),
     Err(e) => Err(e.to_string()),
   };
-}
-
-/// Wizard init handler
-#[tauri::command]
-pub fn init_swarm(swarm_path: String, swarm_persona: String, source_path: String) -> String {
-  let swarm_path = PathBuf::from(&swarm_path);
-  let persona_dir = swarm_path.join("0"); // TODO: alice has directory swarm_path/0, bob 1, carol 2... hard-coding this for demo.
-  let source_path = PathBuf::from(&source_path); // TODO: this is not necessary for swarm, but lib requires it.
-  initialize_host_swarm(swarm_path, persona_dir, Some(swarm_persona), &Some(source_path));
-
-  "ok".to_string()
-}
-
-/// Wizard init handler
-#[tauri::command]
-pub fn swarm_process() -> bool {
-  check_process("diem-swarm")
-}
-
-fn check_process(process_str: &str) -> bool {
-    // get processes from sysinfo
-    let mut system = sysinfo::System::new_all();
-    system.refresh_all();
-    for (_, process) in system.get_processes() {
-        if process.name() == process_str {
-            // TODO: doesn't always catch `miner` running, see get by name below.
-            return true;
-        }
-    }
-    // also try by name (yield different results), most reliable.
-    let p = system.get_process_by_name(process_str);
-    !p.is_empty()
-}
-
-/// Wizard init handler
-#[tauri::command]
-pub fn swarm_files(swarm_dir: String) -> String {
-  let swarm_path = Path::new(&swarm_dir);
-
-
-  format!(
-    "{path}: {path_exists}\n
-    /0/0L.toml: {toml_exists},\n
-    /0/blocks/block_0.json: {block_exists}", 
-    path = swarm_dir,
-    path_exists = swarm_path.exists().to_string(),
-    toml_exists = swarm_path.join("0/0L.toml").exists().to_string(),
-    block_exists = swarm_path.join("0/blocks/block_0.json").exists().to_string()
-  )
-
-  // TODO: make this JSON not string
-  // struct Output {
-  //   path_exists: String,
-  //   toml_exists: String
-  //   block_exists: String,
-  // }
-  // let output = Output {
-  //   path_exists: mnemonic_string,
-  //   toml_exists,
-  //   block_exists,
-  // };
-  // return match serde_json::to_string(&output) {
-  //   Ok(t) => Ok(t),
-  //   Err(e) => Err(e.to_string()),
-  // };
-
 }
 
 /// Wizard init handler
@@ -195,15 +127,6 @@ pub fn wizard_user_check(home: String) -> bool {
   check(home_path)
 }
 
-// Get configs from swarm
-pub fn swarm_params(swarm_path: String) -> Result<TxParams, Error> {
-  let txp = get_tx_params_from_swarm(
-    PathBuf::from(swarm_path),
-    "alice".to_string(),
-    false, 
-  ).unwrap();
-  Ok(txp)
-}
 
 #[tauri::command]
 /// mine for swarm
@@ -228,68 +151,4 @@ pub fn demo(config_dir: String, mnemonic: String) -> String {
   txs::commands::demo_cmd::demo_tx(&tx_params, false, None).unwrap();
 
   "Demo tx submitted".to_string()
-}
-
-// fn get_swarm_cfg(config_dir: String) -> AppCfg {
-//   let toml = Path::new(&config_dir).join("0/0L.toml");
-//   config::parse_toml(toml.to_str().unwrap().to_string()).unwrap()
-// }
-
-fn get_cfg(config_dir: &str, is_swarm: bool) -> AppCfg {
-  let mut toml = PathBuf::from(config_dir);
-  if is_swarm { toml.push("0/") };
-  toml.push("0L.toml");
-  config::parse_toml(toml.to_str().unwrap().to_string()).unwrap()
-}
-
-#[tauri::command]
-/// mine for swarm
-pub fn swarm_miner(swarm_dir: String, swarm_persona: String) -> String {
-
-  let tx_params = get_tx_params_from_swarm(
-    PathBuf::from(&swarm_dir),
-    swarm_persona, 
-    false
-  );
-
-  let appcfg = get_cfg(&swarm_dir, true);
-  
-  // TODO(Ping): mine_and_submit(config, tx_params, is_operator)
-  
-  match mine_once(&appcfg) {
-    Ok(b) => {
-      match commit_proof_tx(&tx_params.unwrap(), b.preimage, b.proof, false) {
-          Ok(tx_view) => match eval_tx_status(tx_view) {
-              Ok(r) => format!("Success: Proof committed to chain \n {:?}", r),
-              Err(e) => format!("ERROR: Proof NOT committed to chain, message: \n{:?}", e),
-          },
-          Err(e) => format!("Miner transaction rejected, message: \n{:?}", e),
-      }
-    },
-    Err(e) => format!("Error mining proof, message: {:?}", e),
-  }
-}
-
-// /// Start Node handler
-// #[tauri::command]
-// pub async fn start_node(home: PathBuf) -> Result<bool, String> {
-//   match NodeConfig::load(home) {
-//     Ok(config) => {
-//       libra_node::start(&config, None);
-//       Ok(true)
-//     }
-//     Err(e) => Err(format!("Config was not loaded from: {:?}", e)),
-//   }
-// }
-
-/// Stop Mining handler
-#[tauri::command]
-pub async fn stop_mining() -> bool {
-  true
-}
-
-/// Stop Mining handler
-#[tauri::command]
-pub async fn stop_node() -> bool {
-  true
 }
--- src-tauri/src/commands/swarm.rs
+++ src-tauri/src/commands/swarm.rs
@@ -1,7 +1,14 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::Command;
+use anyhow::Error;
+use ol::config::AppCfg;
+use ol_types::config;
+use sysinfo::{SystemExt, ProcessExt};
 
-
+use ol::commands::init_cmd::initialize_host_swarm;
+use tower::block::mine_once;
+use tower::commit_proof;
+use txs::submit_tx;
 
 use crate::configs::get_cfg;
 
@@ -22,183 +29,119 @@ pub async fn easy_swarm(_path: PathBuf) -> Result<String, String>  {
 }
 
 
-// #[tauri::command]
-// pub async fn start_swarm(libra_node: PathBuf, config_path: Option<String>, num_nodes: usize, num_full_nodes: usize) -> Result<bool,String> {
-//     println!("libra-node: {:?}", libra_node);
-//     libra_logger::Logger::new().init();
+/// Wizard init handler
+#[tauri::command]
+pub fn init_swarm(swarm_path: String, swarm_persona: String, source_path: String) -> String {
+  let swarm_path = PathBuf::from(&swarm_path);
+  let persona_dir = swarm_path.join("0"); // TODO: alice has directory swarm_path/0, bob 1, carol 2... hard-coding this for demo.
+  let source_path = PathBuf::from(&source_path); // TODO: this is not necessary for swarm, but lib requires it.
+  initialize_host_swarm(swarm_path, persona_dir, Some(swarm_persona), &Some(source_path));
 
-//     let mut validator_swarm = LibraSwarm::configure_validator_swarm(
-//         libra_node.as_ref(),
-//         num_nodes,
-//         config_path.clone(),
-//         None,
-//     )
-//         .expect("Failed to configure validator swarm");
+  "ok".to_string()
+}
 
-//     let mut full_node_swarm = if num_full_nodes > 0 {
-//         Some(
-//             LibraSwarm::configure_fn_swarm(
-//                 libra_node.as_ref(),
-//                 None, /* config dir */
-//                 None,
-//                 &validator_swarm.config,
-//                 FullnodeType::ValidatorFullnode,
-//             )
-//                 .expect("Failed to configure full node swarm"),
-//         )
-//     } else {
-//         None
-//     };
-//     validator_swarm
-//         .launch_attempt()
-//         .expect("Failed to launch validator swarm");
-//     if let Some(ref mut swarm) = full_node_swarm {
-//         swarm
-//             .launch_attempt()
-//             .expect("Failed to launch full node swarm");
-//     }
+/// Wizard init handler
+#[tauri::command]
+pub fn swarm_process() -> bool {
+  check_process("diem-swarm")
+}
 
-//     let libra_root_key_path = &validator_swarm.config.libra_root_key_path;
-//     let validator_config = NodeConfig::load(&validator_swarm.config.config_files[0]).unwrap();
-//     let waypoint = validator_config.base.waypoint.waypoint();
+fn check_process(process_str: &str) -> bool {
+    // get processes from sysinfo
+    let mut system = sysinfo::System::new_all();
+    system.refresh_all();
+    for (_, process) in system.get_processes() {
+        if process.name() == process_str {
+            // TODO: doesn't always catch `miner` running, see get by name below.
+            return true;
+        }
+    }
+    // also try by name (yield different results), most reliable.
+    let p = system.get_process_by_name(process_str);
+    !p.is_empty()
+}
 
-//     println!("To run the Libra CLI client in a separate process and connect to the validator nodes you just spawned, use this command:");
+/// Wizard init handler
+#[tauri::command]
+pub fn swarm_files(swarm_dir: String) -> String {
+  let swarm_path = Path::new(&swarm_dir);
 
-//     println!(
-//         "\tcli -u {} -m {:?} --waypoint {} --chain-id {:?}",
-//         format!(
-//             "http://localhost:{}",
-//             validator_config.json_rpc.address.port()
-//         ),
-//         libra_root_key_path,
-//         waypoint,
-//         ChainId::test().id()
-//     );
 
-//     let ports = validator_swarm.config.config_files.iter().map(|config| {
-//         let validator_config = NodeConfig::load(config).unwrap();
-//         let port = validator_config.json_rpc.address.port();
-//         let debug_interface_port = validator_config
-//             .debug_interface
-//             .admission_control_node_debug_port;
-//         (port, debug_interface_port)
-//     });
+  format!(
+    "{path}: {path_exists}\n
+    /0/0L.toml: {toml_exists},\n
+    /0/blocks/block_0.json: {block_exists}", 
+    path = swarm_dir,
+    path_exists = swarm_path.exists().to_string(),
+    toml_exists = swarm_path.join("0/0L.toml").exists().to_string(),
+    block_exists = swarm_path.join("0/blocks/block_0.json").exists().to_string()
+  )
 
-//     let node_address_list = ports
-//         .clone()
-//         .map(|port| format!("localhost:{}", port.0))
-//         .collect::<Vec<String>>()
-//         .join(",");
+  // TODO: make this JSON not string
+  // struct Output {
+  //   path_exists: String,
+  //   toml_exists: String
+  //   block_exists: String,
+  // }
+  // let output = Output {
+  //   path_exists: mnemonic_string,
+  //   toml_exists,
+  //   block_exists,
+  // };
+  // return match serde_json::to_string(&output) {
+  //   Ok(t) => Ok(t),
+  //   Err(e) => Err(e.to_string()),
+  // };
 
-//     println!("To run transaction generator run:");
-//     println!(
-//         "\tcluster-test --mint-file {:?} --swarm --peers {:?} --emit-tx --workers-per-ac 1",
-//         libra_root_key_path, node_address_list,
-//     );
+}
 
-//     let node_address_list = ports
-//         .map(|port| format!("localhost:{}:{}", port.0, port.1))
-//         .collect::<Vec<String>>()
-//         .join(",");
+// Get configs from swarm
+pub fn swarm_params(swarm_path: String) -> Result<submit_tx::TxParams, Error> {
+  let txp = submit_tx::get_tx_params_from_swarm(
+    PathBuf::from(swarm_path),
+    "alice".to_string(),
+    false, 
+  ).unwrap();
+  Ok(txp)
+}
 
-//     println!("To run health check:");
-//     println!(
-//         "\tcluster-test --mint-file {:?} --swarm --peers {:?} --health-check --duration 30",
-//         libra_root_key_path, node_address_list,
-//     );
 
-//     if let Some(ref swarm) = full_node_swarm {
-//         let full_node_config = NodeConfig::load(&swarm.config.config_files[0]).unwrap();
-//         println!("To connect to the full nodes you just spawned, use this command:");
-//         println!(
-//             "\tcli -u {} -m {:?} --waypoint {} --chain-id {}",
-//             format!(
-//                 "http://localhost:{}",
-//                 full_node_config.json_rpc.address.port()
-//             ),
-//             libra_root_key_path,
-//             waypoint,
-//             ChainId::test().id(),
-//         );
-//     }
-
-//     // let faucet = if args.start_faucet {
-//     //     let faucet_port = libra_config::utils::get_available_port();
-//     //     let server_port = validator_swarm.get_client_port(0);
-//     //     println!("Starting faucet service at port: {}", faucet_port);
-//     //     let process = faucet::Process::start(
-//     //         args.faucet_path.as_ref().unwrap().as_ref(),
-//     //         faucet_port,
-//     //         server_port,
-//     //         Path::new(&libra_root_key_path),
-//     //     );
-//     //     println!("Waiting for faucet connectivity");
-//     //     process
-//     //         .wait_for_connectivity()
-//     //         .expect("Failed to start Faucet");
-//     //     Some(process)
-//     // } else {
-//     //     None
-//     // };
-
-//     // if args.start_client {
-//     //     let tmp_mnemonic_file = TempPath::new();
-//     //     tmp_mnemonic_file.create_as_file().unwrap();
-//     //
-//     //     let port = validator_swarm.get_client_port(0);
-//     //     let client = if let Some(ref f) = faucet {
-//     //         client::InteractiveClient::new_with_inherit_io_faucet(
-//     //             args.cli_path.as_ref().unwrap().as_ref(),
-//     //             port,
-//     //             f.mint_url(),
-//     //             waypoint,
-//     //         )
-//     //     } else {
-//     //         client::InteractiveClient::new_with_inherit_io(
-//     //             args.cli_path.as_ref().unwrap().as_ref(),
-//     //             port,
-//     //             Path::new(&libra_root_key_path),
-//     //             &tmp_mnemonic_file.path(),
-//     //             waypoint,
-//     //         )
-//     //     };
-//     //     println!("Loading client...");
-//     //     let _output = client.output().expect("Failed to wait on child");
-//     //     println!("Exit client.");
-//     // } else {
-//         // Explicitly capture CTRL-C to drop LibraSwarm.
-//         let (tx, rx) = std::sync::mpsc::channel();
-//         ctrlc::set_handler(move || {
-//             tx.send(())
-//                 .expect("failed to send unit when handling CTRL-C");
-//         })
-//             .expect("failed to set CTRL-C handler");
-//         println!("CTRL-C to exit.");
-//         rx.recv()
-//             .expect("failed to receive unit when handling CTRL-C");
-//     // }
-
-//     // if let Some(dir) = &args.config_dir {
-//     //     println!("Please manually cleanup {:?} after inspection", dir);
-//     // }
-
-//     println!("Exit libra-swarm.");
-
-//     Ok(true)
+// fn get_swarm_cfg(config_dir: String) -> AppCfg {
+//   let toml = Path::new(&config_dir).join("0/0L.toml");
+//   config::parse_toml(toml.to_str().unwrap().to_string()).unwrap()
 // }
 
-// // #[tauri::command]
-// // pub async fn init_swarm_miner(swarm_path: String, swarm_persona: String, source_path: String) -> Result<bool, String> {
-// //     let args = vec![
-// //         //"-c".to_string(), "/Users/ping/.0L".to_string(),
-// //         "-s".to_string(), swarm_path,
-// //         "-S".to_string(), swarm_persona,
-// //         "init".to_string(),
-// //         //*"--source-path".to_string(), source_path
-// //     ];
+fn get_swarm_cfg(config_dir: &str, is_swarm: bool) -> AppCfg {
+  let mut toml = PathBuf::from(config_dir);
+  if is_swarm { toml.push("0/") };
+  toml.push("0L.toml");
+  config::parse_toml(toml.to_str().unwrap().to_string()).unwrap()
+}
 
-// //     println!("args of init miner: {:?}", args);
-// //     Application::run(&APPLICATION, args);
-// //     //process::exit(0);
-// //     Ok(true)
-// // }
+#[tauri::command]
+/// mine for swarm
+pub fn swarm_miner(swarm_dir: String, swarm_persona: String) -> String {
+
+  let tx_params = submit_tx::get_tx_params_from_swarm(
+    PathBuf::from(&swarm_dir),
+    swarm_persona, 
+    false
+  );
+
+  let appcfg = get_swarm_cfg(&swarm_dir, true);
+  
+  // TODO(Ping): mine_and_submit(config, tx_params, is_operator)
+  
+  match mine_once(&appcfg) {
+    Ok(b) => {
+      match commit_proof::commit_proof_tx(&tx_params.unwrap(), b.preimage, b.proof, false) {
+          Ok(tx_view) => match submit_tx::eval_tx_status(tx_view) {
+              Ok(r) => format!("Success: Proof committed to chain \n {:?}", r),
+              Err(e) => format!("ERROR: Proof NOT committed to chain, message: \n{:?}", e),
+          },
+          Err(e) => format!("Miner transaction rejected, message: \n{:?}", e),
+      }
+    },
+    Err(e) => format!("Error mining proof, message: {:?}", e),
+  }
+}
