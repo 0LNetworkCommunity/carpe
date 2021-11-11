943aa30b578840a3131907b9c2dcc55096f30f95
lpgeiger <1364012+lpgeiger@users.noreply.github.com>
2021-11-10 21:51:41 -0500

    menu settings to fix copy paste issue

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,7 +12,7 @@ pub mod carpe_error;
 pub mod seed_peers;
 
 
-use tauri::{Menu, MenuItem, Submenu};
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 use crate::{commands::*};
 
@@ -25,55 +25,26 @@ fn main() {
   } 
     let menu = Menu::new()
     .add_submenu(Submenu::new(
-      "Help",
-      Menu::new().add_item(custom_menu("Learn More")),
+      "Carpe",
+      Menu::new()
+        .add_native_item(MenuItem::About("Carpe".to_string()))
+        .add_native_item(MenuItem::Quit),
     ))
-    .add_native_item(MenuItem::Copy);
-    // .add_submenu(Submenu::new(
-    //   // on macOS first menu is always app name
-    //   "Mr Tagger",
-    //   Menu::new()
-    //     .add_native_item(MenuItem::About("Mr Tagger".to_string()))
-    //     .add_native_item(MenuItem::Separator)
-    //     .add_native_item(MenuItem::Services)
-    //     .add_native_item(MenuItem::Separator)
-    //     .add_native_item(MenuItem::Hide)
-    //     .add_native_item(MenuItem::HideOthers)
-    //     .add_native_item(MenuItem::ShowAll)
-    //     .add_native_item(MenuItem::Separator)
-    //     .add_native_item(MenuItem::Quit),
-    // ))
-    // .add_submenu(Submenu::new(
-    //   "File",
-    //   Menu::new()
-    //     .add_item(custom_menu("Open...").accelerator("cmdOrControl+O"))
-    //     .add_native_item(MenuItem::Separator)
-    //     .add_item(custom_menu("Close").accelerator("cmdOrControl+W"))
-    //     .add_item(custom_menu("Save").accelerator("cmdOrControl+S"))
-    //     .add_item(custom_menu("Save As...").accelerator("shift+cmdOrControl+S")),
-    // ))
-    // .add_submenu(Submenu::new("Edit", {
-    //   let mut menu = Menu::new();
-    //   menu = menu.add_native_item(MenuItem::Undo);
-    //   menu = menu.add_native_item(MenuItem::Redo);
-    //   menu = menu.add_native_item(MenuItem::Separator);
-    //   menu = menu.add_native_item(MenuItem::Cut);
-    //   menu = menu.add_native_item(MenuItem::Copy);
-    //   menu = menu.add_native_item(MenuItem::Paste);
-    //   #[cfg(not(target_os = "macos"))]
-    //   {
-    //     menu = menu.add_native_item(MenuItem::Separator);
-    //   }
-    //   menu = menu.add_native_item(MenuItem::SelectAll);
-    //   menu
-    // }))
-    // .add_submenu(Submenu::new("View", Menu::new()))
-    // .add_submenu(Submenu::new(
-    //   "Window",
-    //   Menu::new()
-    //     .add_native_item(MenuItem::Minimize)
-    //     .add_native_item(MenuItem::Zoom),
-    // ))
+    .add_submenu(Submenu::new("Edit", {
+      let mut menu = Menu::new();
+      menu = menu.add_native_item(MenuItem::Undo);
+      menu = menu.add_native_item(MenuItem::Redo);
+      menu = menu.add_native_item(MenuItem::Separator);
+      menu = menu.add_native_item(MenuItem::Cut);
+      menu = menu.add_native_item(MenuItem::Copy);
+      menu = menu.add_native_item(MenuItem::Paste);
+      #[cfg(not(target_os = "macos"))]
+      {
+        menu = menu.add_native_item(MenuItem::Separator);
+      }
+      menu = menu.add_native_item(MenuItem::SelectAll);
+      menu
+    }));
 
 
 	tauri::Builder::default()
