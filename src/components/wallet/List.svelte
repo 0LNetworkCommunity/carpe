417824d728111c675efe0e511c4aba581edc170f
soaresa <10797037+soaresa@users.noreply.github.com>
2021-11-14 14:26:51 -0300

    Highlights account mining in wallet list

--- src/components/wallet/List.svelte
+++ src/components/wallet/List.svelte
@@ -7,6 +7,7 @@
     setAccount,
   } from "../../accounts";
   import type { AccountEntry } from "../../accounts";
+  import { miner_loop_enabled } from "../../miner";
   import { onMount } from "svelte";
   import { Link } from "svelte-navigator";
   import { get_balance } from "../../queries";
@@ -16,18 +17,19 @@
   UIkit.use(Icons);
 
   let account_list: AccountEntry[] = [];
-  let pendingAccounts: AccountEntry[] = [];
-
-  let my_account: AccountEntry;
-
+  let pendingAccounts: AccountEntry[] = []; 
   all_accounts.subscribe((a) => {
     account_list = a;
     pendingAccounts = a.filter(x => !x.balance);
   });
-
+  
+  let my_account: AccountEntry;
   signingAccount.subscribe((a) => {
     my_account = a;
   });
+  
+  let isMining = false; 
+  miner_loop_enabled.subscribe(boo => isMining = boo);
 
   async function bal(i): Promise<number> {
     let n = await get_balance(account_list[i]);
@@ -66,13 +68,23 @@
       <tbody>
         {#each account_list as a, i}
           <tr
-            class="{my_account.account == a.account ? 'uk-text-primary' : ''}"
+            class="{
+              my_account.account == a.account 
+                ? isMining
+                  ? 'uk-text-success' 
+                  : 'uk-text-primary'
+                : ''
+              }"
             on:click={() => setAccount(a.account) }
           >
             <!-- <a href="#" on:click={() => { setAccount(acc.account); }}> {acc.nickname} </a > -->
             <td>
               {#if a.account == my_account.account}
-                <span uk-icon="user" />
+                {#if isMining} 
+                  <img alt="mining icon" src="/images/mining.png">
+                {:else}
+                  <span uk-icon="user" />
+                {/if}
               {/if}
             </td>
             <td>{a.nickname}</td>
