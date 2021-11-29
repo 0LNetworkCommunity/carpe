c03486ee3bf42603333e0d3e9e3dfb28fecd977a
Jarryd <16006178+JarrydTrokis@users.noreply.github.com>
2021-11-29 19:44:07 +0000

    chore(routes): Adds routes config

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+export const routes = {
+  home: '/',
+  addAccount: '/add-account',
+  accountFromMnem: '/account-from-mnem',
+  keygen: '/keygen',
+  miner: '/miner',
+  transactions: '/txs',
+  settings: '/settings',
+  developer: '/dev',
+  swarm: '/swarm'
+}
