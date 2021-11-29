e1fa4db7fc7d4ae307ab2c6c967f22ed3495d1de
Jarryd <16006178+JarrydTrokis@users.noreply.github.com>
2021-11-29 19:43:57 +0000

    feat(nav) Adds back button to settings

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-  import { Link } from "svelte-navigator";
+  import { Link, useLocation } from "svelte-navigator";
   import { debugMode } from "../debug";
   import { signingAccount, all_accounts } from "../accounts";
   import AccountSwitcher from "./wallet/AccountSwitcher.svelte";
+  import { routes } from "../routes";
 
+  const location = useLocation();
+  
   let debug = false;
   debugMode.subscribe((d) => {
     debug = d;
@@ -32,19 +35,21 @@
 
 <main class="uk-margin-top">
   <nav class="uk-navbar-container" uk-navbar>
+    {#if $location.pathname === routes.settings}
+      <Link to={routes.home}><span uk-icon="icon: arrow-left; ratio: 2" /></Link>
+    {/if}
+
     <div class="uk-navbar-center">
       <ul class="uk-navbar-nav">
         {#if myAccountIsOnChain && has_account}
-          <li><Link to="/">Wallet</Link></li>        
-          <li><Link to="miner">Miner</Link></li>
-          <li><Link to="txs">Transactions</Link></li>
+          <li><Link to={routes.home}>Wallet</Link></li>        
+          <li><Link to={routes.miner}>Miner</Link></li>
+          <li><Link to={routes.transactions}>Transactions</Link></li>
         {/if}
 
-        <!-- <li><Link to="settings">Settings</Link></li> -->
         {#if debug}
-          <li><Link to="dev">Debug</Link></li>
+          <li><Link to={routes.developer}>Debug</Link></li>
         {/if}
-        <!-- <li><Link to="swarm">Swarm</Link></li> -->
       </ul>
     </div>
 
