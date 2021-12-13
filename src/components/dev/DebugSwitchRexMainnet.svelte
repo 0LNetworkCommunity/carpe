0a6489521009d480f5782afcc14b0ba801016509
soaresa <10797037+soaresa@users.noreply.github.com>
2021-12-13 05:21:20 -0300

    Fixes debug network check box list behavior

--- src/components/dev/DebugSwitchRexMainnet.svelte
+++ src/components/dev/DebugSwitchRexMainnet.svelte
@@ -14,7 +14,7 @@
 <div class="uk-margin-medium-bottom">
   <h4 class="uk-text-light uk-text-uppercase uk-text-muted uk-text-thin"> Network Connection</h4>
   <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
-    <label><input class="uk-radio" type="radio" name="radio2" checked={current_chain_id  == "Mainnet"} on:click={() => setNetwork(Networks.Mainnet)}> Mainnet </label>
-    <label><input class="uk-radio" type="radio" name="radio2" checked={current_chain_id == "Rex"} on:click={() => setNetwork(Networks.Rex)}> Rex (testnet) </label>
+    <label><input class="uk-radio" type="radio" name="networkCb" checked={current_chain_id == "Mainnet"} on:click={() => setNetwork(Networks.Mainnet)}> Mainnet </label>
+    <label><input class="uk-radio" type="radio" name="networkCb" checked={current_chain_id == "Rex"} on:click={() => setNetwork(Networks.Rex)}> Rex (testnet) </label>
   </div>
 </div>
