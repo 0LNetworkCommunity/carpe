48c1f7cf73306c5cffd4bab35e880ab524a8eadf
liangping <18786721@qq.com>
2022-02-26 20:59:57 +0800

    preload lang

--- src/components/layout/LocaleSelector.svelte
+++ src/components/layout/LocaleSelector.svelte
@@ -16,6 +16,12 @@
       <select value={value} on:change={handleLocaleChange}>
         <option value="en">English</option>
         <option value="zh_cn">中文</option>
+        <option value="pt">Portuguese</option>
+        <option value="de">German</option>
+        <option value="it">Italian</option>
+        <option value="es">Spanish</option>
+        <option value="ar">Arabic</option>
+        <option value="fr">French</option>
       </select>
     </div>
   </div>
--- src/lang/i18n.ts
+++ src/lang/i18n.ts
@@ -9,9 +9,21 @@ import {
 
 import en from './locales/en.json'
 import zh_cn from './locales/zh_cn.json'
+import de from './locales/de.json'
+import fr from './locales/fr.json'
+import es from './locales/es.json'
+import it from './locales/it.json'
+import pt from './locales/pt.json'
+import ar from './locales/ar.json'
 
 register('en', async () => en);
 register('zh_cn', async ()=> zh_cn);
+register('de', async () => de);
+register('fr', async () => fr);
+register('es', async () => es);
+register('it', async () => it);
+register('pt', async () => pt);
+register('ar', async () => ar);
 
 function setupI18n(options) {
   const { withLocale: locale_ } = options;
