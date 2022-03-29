9ac4665ce6523460eaaef4b2b333597853887e36
soaresa <10797037+soaresa@users.noreply.github.com>
2022-03-29 16:03:51 -0300

    Optimizes Miner components subscriptions

--- src/components/miner/TowerState.svelte
+++ src/components/miner/TowerState.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import { _ } from "svelte-i18n";
-  import { onMount } from "svelte";
-  import { tower } from "../../miner";
 
   export let minerTower;
   
@@ -61,7 +59,5 @@
     </table>
     
     <!-- <EpochStatus actual_proofs={minerTower.on_chain.actual_count_proofs_in_epoch} /> -->
-  {:else}
-    <TowerStateSkeleton />
   {/if}
 </main>
