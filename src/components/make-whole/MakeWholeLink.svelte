e2562ad63936dcefd6576e2eb4643b9a862ea6e8
soaresa <10797037+soaresa@users.noreply.github.com>
2022-04-29 18:20:39 -0300

    Apply language framework to make whole  (#111)
    * Apply language framework to make whole

--- src/components/make-whole/MakeWholeLink.svelte
+++ src/components/make-whole/MakeWholeLink.svelte
@@ -1,16 +1,22 @@
 <script>
-  import { onMount } from "svelte";
+  import { onDestroy, onMount } from 'svelte';
   import { Link } from "svelte-navigator";
   import { makeWhole } from "../../accounts";
   import { routes } from "../../routes";
+  import { _ } from "svelte-i18n";
 
+  let unsubs;
   let hasMakeWhole = false;
   let hasCoinsToClaim = false;
   onMount(async () => {
-    makeWhole.subscribe(mk => {
+    unsubs = makeWhole.subscribe(mk => {
       hasMakeWhole = mk && Object.values(mk).find(credits => credits.length > 0);
       hasCoinsToClaim = mk && Object.values(mk).find(credits => credits.find(credit => !credit.claimed))
     });
+  })
+
+  onDestroy(async () => {
+    unsubs && unsubs();
   })
 
 </script>
@@ -19,7 +25,7 @@
   <Link to={routes.makeWhole}>
     <button class="uk-button {hasCoinsToClaim ? "uk-button-primary" : "uk-button-default"}"> 
       <span uk-icon="icon: warning; ratio: 0.8" style="margin-right: 5px;"></span>
-      Claim Coins
+      {$_("make_whole.link_title")}
     </button>
   </Link>
 {/if}
