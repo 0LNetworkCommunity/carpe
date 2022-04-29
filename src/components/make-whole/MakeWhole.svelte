a17ab8997b03fb665b0dec76281fc1e5d039db7d
soaresa <10797037+soaresa@users.noreply.github.com>
2022-04-29 18:15:08 -0300

    Apply language framework to make whole

--- src/components/make-whole/MakeWhole.svelte
+++ src/components/make-whole/MakeWhole.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-  import { _ } from 'svelte-i18n';
   import UIkit from 'uikit';
   import { printCoins } from '../../coinHelpers';
   import { makeWhole } from "../../accounts";
   import { onDestroy, onMount } from 'svelte';
   import { claimMakeWhole } from '../../accountActions';
+  import { _ } from "svelte-i18n";
     
   let credits;
   let unsubs;
@@ -49,7 +49,7 @@
     callback.bind({isProcessing});
     claimMakeWhole(selected.account, callback);
   }
-
+//on:click={() => { selected = credit; UIkit.modal('#claimedWithSuccess').show()}}
 </script>
 
 <main>
@@ -57,16 +57,16 @@
     <div class="uk-modal-dialog uk-modal-body uk-text-center" style="background-image: url('/images/confetti.gif')">
       <div class="uk-section">
         <h2 class="uk-modal-title"><span class="success-icon" uk-icon="icon: check; ratio: 2"></span></h2> 
-        <p class="uk-text-small">Transation confirmed!</p>
-        <p>You have claimed <span class="uk-text-bold">{selected && printCoins(selected.coins.value)} coins</span> for account <br><span class="uk-text-bold">{selected ? selected.account : ""}</span>.</p>
-        <p>You can check your balance now.</p>
+        <p class="uk-text-small">{$_("make_whole.modal_success.title")}</p>
+        {@html $_("make_whole.modal_success.amount_for_account", { values: { coins: selected && printCoins(selected.coins.value), account: selected ? selected.account : '' } })}
+        <p>{$_("make_whole.modal_success.check_balance")}</p>
       </div>
 
       <p class="uk-text-center">
         <button 
           class="uk-button uk-button-large uk-button-primary uk-margin-right uk-modal-close" 
           type="button"
-        >OK</button>
+        >{$_("make_whole.modal_success.ok_btn")}</button>
       </p>
     </div>
   </div>
@@ -74,25 +74,23 @@
   <div id="claimError" uk-modal>
     <div class="uk-modal-dialog uk-modal-body uk-text-center">
       <div class="uk-section">
-        <h2 class="uk-modal-title">Claim Error</h2> 
-        <p>You must turn off miner to claim this account coins.</p>
+        <h2 class="uk-modal-title"> {$_("make_whole.modal_error.title")}</h2> 
+        <p>{errorMsg}</p>
       </div>
 
       <p class="uk-text-center">
         <button 
           class="uk-button uk-button-large uk-button-primary uk-margin-right uk-modal-close" 
           type="button"
-        >OK</button>
+        >{$_("make_whole.modal_error.ok_btn")}</button>
       </p>
     </div>
   </div>
 
   <div class="uk-container">
     <div class="uk-card uk-card-default uk-card-body">
-      <h3 class="uk-card-title">Claim Your Missing Coins</h3>
-      <p>Oops. Occasionally blockchains make math mistakes. 0L tries to fix them as quickly as possible.</p>
-      <p>Miner Identity Subsidy Incident: From Epoch 0 to epoch 52, the payouts to miners was lower than expected. Below you can credit your missing coins from that period.</p>
-      <p>Click <a href="">here</a> to learn more.</p>
+      <h3 class="uk-card-title">{$_("make_whole.card.title")}</h3>
+      {@html $_("make_whole.card.body")}
     </div>
     <div>
       
@@ -101,9 +99,9 @@
       <table class="uk-table uk-table-divider">
         <thead>
           <tr>
-            <th class="uk-text-left">Account</th>
-            <th class="uk-text-right">Amount</th>
-            <th class="uk-text-center">Claim</th>
+            <th class="uk-text-left">{$_("make_whole.table.account")}</th>
+            <th class="uk-text-right">{$_("make_whole.table.amount")}</th>
+            <th class="uk-text-center">{$_("make_whole.table.claim")}</th>
           </tr>
         </thead>
         <tbody>
@@ -113,14 +111,16 @@
               <td class="uk-text-right">{printCoins(credit.coins.value)}</td>
               <td class="uk-text-center" style="width: 200px;">
                 {#if credit.claimed}
-                  <span class="uk-text-success" uk-icon="icon: check; ratio: 1; color: green"></span>
+                  <span 
+                    on:click={() => { errorMsg = "faiou"; UIkit.modal('#claimError').show(); }}
+                    class="uk-text-success" uk-icon="icon: check; ratio: 1; color: green"></span>
                 {:else}
                   <button 
                     on:click={() => claimCoins(credit)}
                     disabled={isProcessing}
                     style="width: 180px;"
                     class="uk-button uk-button-primary"
-                  >{isProcessing ? "Await..." : "Claim Now"}</button>
+                  >{isProcessing ? $_("make_whole.claim_btn.await") : $_("make_whole.claim_btn.claim")}</button>
                 {/if}
               </td>
             </tr>
@@ -128,7 +128,7 @@
         </tbody>
       </table>
     {:else}
-      loading...
+      <span uk-spinner style="position:absolute; top:0px; left:0px"/>
     {/if}
   </div>
 </main>
