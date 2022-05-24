5699f9958cee48e661c428a6a9fdffccedb240a5
soaresa <10797037+soaresa@users.noreply.github.com>
2022-05-24 12:28:11 -0300

    Fix fullnodes scan (#124)
    * Fix rust code getting best fullnode url
    
    * Wait for account to be loaded before showing scanning for fullnodes
    
    * Hides make whole navbar link

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -6,7 +6,7 @@
   import { routes } from "../routes";
   import { _ } from "../lang/i18n";
   import { init_preferences } from "../preferences";
-  import MakeWholeLink from "./make-whole/MakeWholeLink.svelte";
+  // import MakeWholeLink from "./make-whole/MakeWholeLink.svelte";
   
   init_preferences();
 
@@ -45,7 +45,8 @@
         <li><Link to={routes.miner}>{$_("nav.miner")}</Link></li>
         <li><Link to={routes.transactions}>{$_("nav.transactions")}</Link></li>
         <li><Link to={routes.events}>{$_("nav.events")}</Link></li>
-        <li><MakeWholeLink /></li>
+        <!-- Postpone MakeWhole release -->
+        <!--<li><MakeWholeLink /></li>-->
       </ul>
     </div>
 
