acfe3a9efaf548359a64f8121ecc60f6a62bf0f2
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-06-28 16:13:04 -0400

    patch issue with force upstream

--- src-tauri/src/commands/networks.rs
+++ src-tauri/src/commands/networks.rs
@@ -3,13 +3,10 @@
 use crate::configs::get_cfg;
 use crate::{
   carpe_error::CarpeError,
-  // configs_network::{
-  //   override_upstream_node, set_network_configs,
-  //   NetworkPlaylist,
-  // }, 
 };
 use libra_types::exports::{NamedChain};
 use libra_types::legacy_types::network_playlist::NetworkPlaylist;
+use libra_types::legacy_types::network_playlist::HostProfile;
 use url::Url;
 
 #[tauri::command(async)]
@@ -35,8 +32,12 @@ pub async fn override_playlist(url: Url) -> Result<NetworkPlaylist, CarpeError> 
 /// we want to elimated the entire playlist and use a single fullnode
 pub async fn force_upstream(url: Url) -> Result<NetworkPlaylist, CarpeError> {
   let mut app_cfg = get_cfg()?;
-  let playlist = NetworkPlaylist::from_url(url, Some(app_cfg.chain_info.chain_id)).await?;
+  dbg!(&app_cfg);
+  let mut dummy_playlist = NetworkPlaylist::default();
+  dummy_playlist.nodes = vec![HostProfile::new(url)];
+  dbg!(&dummy_playlist);
 
-  app_cfg.network_playlist = Some(vec![playlist.clone()]);
-  Ok(playlist)
+  app_cfg.network_playlist = Some(vec![dummy_playlist.clone()]);
+  dbg!(&app_cfg);
+  Ok(dummy_playlist)
 }
