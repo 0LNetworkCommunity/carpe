e2d3e077678a819e51e35197da402a37339870f5
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-06-29 16:21:31 -0400

    css tweaks

--- src/carpeNotify.ts
+++ src/carpeNotify.ts
@@ -5,9 +5,9 @@ import UIkit from 'uikit';
 export function notify_success(s: string) {
   UIkit.notification({
     message: `<span uk-icon=\'icon: check\'></span> ${s}`,
-    pos: 'bottom-center',
+    pos: 'bottom-left',
     status: 'success',
-    timeout: 3000
+    timeout: 10000
   });
 }
 
@@ -15,8 +15,8 @@ export function notify_success(s: string) {
 export function notify_error(s: string) {
   UIkit.notification({
     message: `<span uk-icon=\'icon: warning\'></span> ${s}`,
-    pos: 'bottom-center',
+    pos: 'bottom-left',
     status: 'error',
-    timeout: 10000
+    timeout: 15000
   });
 }
