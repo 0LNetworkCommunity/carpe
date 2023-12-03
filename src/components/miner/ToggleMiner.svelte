e51ea6dd34942e98bc3f6c32624bf0a1bebbbf25
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2023-12-02 20:11:24 -0500

    don't destroy miner interval when leaving miner page

--- src/components/miner/ToggleMiner.svelte
+++ src/components/miner/ToggleMiner.svelte
@@ -1,30 +1,12 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte'
-  import { toggleMining } from '../../modules/miner_toggle'
   import { minerLoopEnabled } from '../../modules/miner'
-    import { setProofPercent } from '../../modules/miner_invoke'
-
-  let enabled: boolean
-  let unsubscribe
-  let looper
-
-  onMount(async () => {
-    unsubscribe = minerLoopEnabled.subscribe((boo) => (enabled = boo))
-    // for safety clear the interval
-    clearInterval(looper)
-    looper = setInterval(() => setProofPercent(), 1000)
-  })
-
-  onDestroy(async () => {
-    clearInterval(looper)
-    unsubscribe && unsubscribe()
-  })
+  import { toggleMining } from '../../modules/miner_toggle'
 </script>
 
 <main>
   <div class="uk-text-center uk-margin" style="position: relative">
     <label class="uk-switch">
-      <input type="checkbox" on:click={() => toggleMining()} checked={enabled} />
+      <input type="checkbox" on:click={() => toggleMining()} checked={$minerLoopEnabled} />
       <div class="uk-switch-slider uk-switch-on-off round" />
     </label>
   </div>
