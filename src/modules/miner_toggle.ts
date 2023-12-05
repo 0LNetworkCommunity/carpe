9ab748ab709ff60610bacfc7cf5d6221349993c3
0o-de-lally <>
2023-12-05 12:58:01 -0500

    Squashed commit of the following:
    commit 9cf0768080f81013117fbd2519eaa79db4a4f2e3
    Author: 0o-de-lally <>
    Date:   Tue Dec 5 12:31:58 2023 -0500
    
        extra attempt to parse v5 address on transfer
    
    commit 37d60ebdef19acb432f6a4e817bebd999904c724
    Author: 0o-de-lally <>
    Date:   Tue Dec 5 12:04:29 2023 -0500
    
        update dep: keyring
    
    commit 51d09340d08509b408a89e88d013d90323b5e1b5
    Author: 0o-de-lally <>
    Date:   Tue Dec 5 11:40:38 2023 -0500
    
        miner progress bar FML
    
    commit 086bc5ea340aae3d15b5dcd9cbe16cf214dd53c2
    Author: 0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
    Date:   Sat Dec 2 20:12:22 2023 -0500
    
        lint
    
    commit e51ea6dd34942e98bc3f6c32624bf0a1bebbbf25
    Author: 0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
    Date:   Sat Dec 2 20:11:24 2023 -0500
    
        don't destroy miner interval when leaving miner page

--- src/modules/miner_toggle.ts
+++ src/modules/miner_toggle.ts
@@ -1,7 +1,14 @@
 import { get } from 'svelte/store'
 import { minerLoopEnabled } from './miner'
-import { killBacklogListener, startBacklogListener, maybeTowerOnce } from './miner_invoke'
+import {
+  killBacklogListener,
+  startBacklogListener,
+  maybeTowerOnce,
+  setProofPercent,
+} from './miner_invoke'
 import { carpeTick } from './tick'
+
+let looper
 
 export const enableMining = async (): Promise<void> => {
   if (!get(minerLoopEnabled)) {
@@ -14,10 +21,13 @@ export const enableMining = async (): Promise<void> => {
     // we should only ask for this once. We ask for it when the user toggles the miner.
     // if the user has stuck proofs, we will show an error screen with a button for
     // manually triggering the backlog.
+
     startBacklogListener()
       .then(carpeTick)
       .then(() => minerLoopEnabled.set(true))
       .then(maybeTowerOnce)
+
+    looper = setInterval(() => setProofPercent(), 5000)
   }
 }
 
@@ -26,6 +36,7 @@ export async function disableMining(): Promise<boolean> {
   // set mining to disabled
   minerLoopEnabled.set(false)
   killBacklogListener() // TODO: how do we prevent zombie listeners from making duplicates.
+  clearInterval(looper)
   return true
 }
 
