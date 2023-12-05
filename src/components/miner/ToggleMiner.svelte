9d87fb8e4b2093f5c9daf212d2bb5db0f44ef59b
0o-de-lally <>
2023-12-05 13:16:40 -0500

    responsive miner toggle

--- src/components/miner/ToggleMiner.svelte
+++ src/components/miner/ToggleMiner.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
   import { minerLoopEnabled } from '../../modules/miner'
   import { toggleMining } from '../../modules/miner_toggle'
+
+  let checking = false
+
+  const toggle = () => {
+    checking = true
+    setTimeout(() => (checking = false), 1000)
+    toggleMining()
+  }
 </script>
 
 <main>
   <div class="uk-text-center uk-margin" style="position: relative">
-    <label class="uk-switch">
-      <input type="checkbox" on:click={() => toggleMining()} checked={$minerLoopEnabled} />
-      <div class="uk-switch-slider uk-switch-on-off round" />
-    </label>
+    {#if !checking}
+      <label class="uk-switch">
+        <input type="checkbox" on:click={toggle} checked={$minerLoopEnabled} />
+        <div class="uk-switch-slider uk-switch-on-off round" />
+      </label>
+    {:else}
+      <div uk-spinner></div>
+    {/if}
   </div>
 </main>
