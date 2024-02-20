335242053b9dce6ec62729a907380e328bf3596d
teisnilsson <97843018+Teisson@users.noreply.github.com>
2024-02-20 14:58:12 +0100

    Update networks.ts
    Added fallback nodes and modified initNetwork for better functionality and enhanced test flows
    And modified the code to follow better practices for the defaultPlaylist and updateNetwork functions, to hopefully run better and not be caught by the linters

--- src/modules/networks.ts
+++ src/modules/networks.ts
@@ -1,29 +1,29 @@
-import { invoke } from '@tauri-apps/api/tauri';
-import { get, writable } from 'svelte/store';
-import { Level, logger, raise_error } from './carpeError';
-import type { CarpeError } from './carpeError';
-import { refreshAccounts } from './accountActions';
-import { notify_success } from './carpeNotify';
-import { nodeEnvIsTest } from './debug';
+import { invoke } from '@tauri-apps/api/tauri'
+import { get, writable } from 'svelte/store'
+import { Level, logger, raise_error } from './carpeError'
+import type { CarpeError } from './carpeError'
+import { refreshAccounts } from './accountActions'
+import { notify_success } from './carpeNotify'
+import { nodeEnvIsTest } from './debug'
 
 // Matches Rust equivalent
 export interface NetworkPlaylist {
-  chain_id: NamedChain;
-  nodes: HostProfile[];
+  chain_id: NamedChain // Todo, use the Network Enum
+  nodes: HostProfile[]
 }
 
 // matches rust equivalent
 export interface HostProfile {
-  url: string;
-  note: string;
-  version: number;
-  is_api: boolean;
-  is_sync: boolean;
+  url: string
+  note: string
+  version: number
+  is_api: boolean
+  is_sync: boolean
 }
 
 // Default playlist which is provided in Carpe.
 export const playlistJsonUrl =
-  'https://raw.githubusercontent.com/0LNetworkCommunity/seed-peers/main/fullnode_seed_playlist.json';
+  'https://raw.githubusercontent.com/0LNetworkCommunity/seed-peers/main/fullnode_seed_playlist.json'
 
 // Embedded default node list as a fallback
 const embeddedNodeList: HostProfile[] = [
@@ -31,56 +31,56 @@ const embeddedNodeList: HostProfile[] = [
     note: "mainnet-rpc",
     url: "https://rpc.openlibra.space:8080/",
     version: 1,
-    is_api: true,
-    is_sync: true,
+    is_api: false,
+    is_sync: false,
   },
   {
     note: "sirouk",
     url: "http://70.15.242.6:8080",
     version: 1,
-    is_api: true,
-    is_sync: true,
+    is_api: false,
+    is_sync: false,
   },
   {
     note: "Alan Yoon",
     url: "http://222.101.31.242:8080",
     version: 1,
-    is_api: true,
-    is_sync: true,
+    is_api: false,
+    is_sync: false,
   },
   {
     note: "Bethose | SDL",
     url: "http://65.109.80.179:8080",
     version: 1,
-    is_api: true,
-    is_sync: true,
+    is_api: false,
+    is_sync: false,
   }
-];
+]
 
 // Function to fetch the node list from the primary source
 async function fetchNodeList(): Promise<HostProfile[]> {
   try {
-    const response = await fetch(playlistJsonUrl);
-    if (!response.ok) throw new Error('Failed to fetch');
-    const data = await response.json();
-    return data.nodes;
+    const response = await fetch(playlistJsonUrl)
+    if (!response.ok) throw new Error('Failed to fetch')
+    const data = await response.json()
+    return data.nodes
   } catch (error) {
-    console.error('Error fetching node list from playlistJsonUrl, using embedded list:', error);
-    return embeddedNodeList;
+    console.error('Error fetching node list from playlistJsonUrl, using embedded list:', error)
+    return embeddedNodeList
   }
 }
 
 // Chain metadata matches the index of node api
 export interface IndexResponse {
-  chain_id: number;
-  epoch: number;
-  ledger_version: number;
-  oldest_ledger_version: number;
-  ledger_timestamp: number;
-  node_role: string;
-  oldest_block_height: number;
-  block_height: number;
-  git_hash: string;
+  chain_id: number
+  epoch: number
+  ledger_version: number
+  oldest_ledger_version: number
+  ledger_timestamp: number
+  node_role: string
+  oldest_block_height: number
+  block_height: number
+  git_hash: string
 }
 
 // This matches a subset of NamedChain enum in Rust.
@@ -135,7 +135,7 @@ nodeEnvIsTest.subscribe((value) => {
 
 // Initialize and update the network settings based on fetched data
 export const initNetwork = async () => {
-  logger(Level.Info, 'initNetwork');
+  logger(Level.Info, 'initNetwork')
   if (!isTest) {
     const nodeList = await fetchNodeList(); // Fetch node list with fallback
     const updatedNetworkPlaylist: NetworkPlaylist = {
@@ -145,8 +145,8 @@ export const initNetwork = async () => {
     await updateNetwork(JSON.stringify(updatedNetworkPlaylist), false);
     await getNetwork(); // Refresh network settings
     if (current_network_profile.chain_id === NamedChain.TESTING) {
-      logger(Level.Info, 'Network set to TESTING mode');
+      logger(Level.Info, 'Network set to TESTING mode')
     }
   }
-  return true;
-};
+  return true
+}
