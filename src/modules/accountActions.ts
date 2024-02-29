87de340e5653fa94a0fb86332301217ce64f735a
bbk <chnqbj@gmail.com>
2024-02-29 16:36:13 +0800

    fix: add missing code

--- src/modules/accountActions.ts
+++ src/modules/accountActions.ts
@@ -36,7 +36,6 @@ export const getAccounts = async () => {
   invoke('get_all_accounts')
     .then((result: CarpeProfile[]) => {
       allAccounts.set(result)
-      console.log(result)
     })
     .catch((e) => raise_error(e, true, 'get_all_accounts'))
 }
