278bd2388b49adf5167502c001bebf9e917db608
soaresa <10797037+soaresa@users.noreply.github.com>
2024-05-30 11:37:03 -0300

    improves remove all accounts confirmation messages moves remove account texts to the language file

--- src/components/settings/AccountSettings.svelte
+++ src/components/settings/AccountSettings.svelte
@@ -2,17 +2,28 @@
   import { _ } from 'svelte-i18n'
   import { removeAllAccounts } from '../../modules/accountActions'
   import { writable } from 'svelte/store'
+  import { onMount } from 'svelte'
+  import UIkit from 'uikit'
 
   let isSubmitting = writable(false)
+  let modal 
 
-  const removeAccounts = async () => {
+  onMount(() => {
+    modal = UIkit.modal("#removeAllAccountsModal")
+  })
+
+  const handleRemoveAllAccountsClick = () => {
+    modal.show()
+  }
+
+  const handleConfirmRemoveAllAccounts = async () => {
     isSubmitting.set(true)
     try {
       removeAllAccounts() 
     } finally {
-      isSubmitting.set(false)
+      modal.hide()
+      isSubmitting.set(false)      
     }
-    
   }
 </script>
 
@@ -24,10 +35,14 @@
   <div class="uk-margin" uk-grid>
     <div>
       <div class="uk-inline">
-        <button class="uk-button uk-button-danger" type="button"
-          >{$_('settings.account_settings.btn_remove')}</button
+        <button 
+          class="uk-button uk-button-danger" 
+          type="button"
+          on:click|preventDefault={handleRemoveAllAccountsClick}
         >
-        <div uk-dropdown="mode: click">
+          {$_('settings.account_settings.btn_remove')}
+        </button>
+        <!--<div uk-dropdown="mode: click">
           <p>{$_('settings.account_settings.confirm')}</p>
           <button class="uk-button uk-button-danger" 
             on:click={removeAccounts}
@@ -35,13 +50,50 @@
             >
             {$_('settings.account_settings.btn_remove')}
           </button>
-        </div>
+        
+          </div>
+        -->
       </div>
     </div>
     <div>
       <span>
         {$_('settings.account_settings.description')}
       </span>
+    </div>
+  </div>
+
+  <!-- REMOVE ALL ACCOUNTS MODAL -->
+  <div id="removeAllAccountsModal" uk-modal class="uk-modal-container">
+    <div class="uk-modal-dialog uk-modal-body uk-padding-large">
+      <div>
+        <h2 class="uk-modal-title uk-text-uppercase uk-text-danger">
+          {$_("settings.remove_all_accounts.modal.title")}
+        </h2>
+        <h3 class="uk-text-bold">{$_("settings.remove_all_accounts.modal.body.question")}</h3>
+        <p class="uk-text-lead uk-text-warning">{$_("settings.remove_all_accounts.modal.body.review_list.title")}</p>
+        <ul class="uk-list uk-list-bullet uk-text-left uk-margin-top uk-margin-large-bottom">
+          <li>{@html $_("settings.remove_all_accounts.modal.body.review_list.item1")}</li>
+          <li>{@html $_("settings.remove_all_accounts.modal.body.review_list.item2")}</li>
+          <li>{@html $_("settings.remove_all_accounts.modal.body.review_list.item3")}</li>
+          <li>{@html $_("settings.remove_all_accounts.modal.body.review_list.item4")}</li>
+        </ul>
+      </div>        
+      <div class="uk-margin-top uk-text-right">
+        <button 
+          class="uk-button uk-button-default uk-margin-right uk-modal-close"
+          disabled={!isSubmitting}
+        >
+        {$_("settings.remove_all_accounts.modal.btn_cancel")}
+        </button>
+        <button 
+          type="button"
+          class="uk-button uk-button-danger" 
+          disabled={!isSubmitting}
+          on:click|preventDefault={handleConfirmRemoveAllAccounts}
+        >
+        {$_("settings.remove_all_accounts.modal.btn_confirm")}
+        </button>
+      </div>
     </div>
   </div>
 </main>
--- src/components/wallet/AccountDetails.svelte
+++ src/components/wallet/AccountDetails.svelte
@@ -4,53 +4,15 @@
   import AccountNote from './AccountNote.svelte';
   import SetWalletType from '../txs/SetWalletType.svelte';
   import GetPrivateKey from './GetPrivateKey.svelte';
-  import { removeAccount } from '../../modules/accountActions';
-  import { navigate } from 'svelte-navigator'
-  import UIkit from 'uikit'
-  import { onMount } from 'svelte'
-  import { writable } from 'svelte/store'
-  //import { _ } from 'svelte-i18n';
-
-  /*
-  function handleKeyRotation() {
-    // Implement key rotation logic
-  }
-  */
-
-  let modal
-
-  onMount(() => {
-    modal = UIkit.modal("#removeAccountModal")
-  })
-
-  function handleRemoveAccount() {
-    if ($signingAccount.watch_only) {
-      removeAccount($signingAccount.account) 
-      navigate('/wallet')
-    } else {
-      modal.show()      
-    }
-  }
-
-  let isRemoving = writable(false)
-  function handleConfirmRemoveAccount() {
-    isRemoving.set(true)
-    try {
-      removeAccount($signingAccount.account) 
-    }
-    finally {
-      isRemoving.set(false)
-      modal.hide()
-      navigate('/wallet')
-    }
-  }
+  import RemoveAccount from './RemoveAccount.svelte';
+  import { _ } from 'svelte-i18n';
 
 </script>
 
 <main class="uk-container uk-margin-large-top">
   <div class="uk-position-relative uk-margin-bottom">
     <div class="uk-flex uk-flex-center">
-      <h2 class="uk-text-light uk-text-muted uk-text-uppercase">Account Details</h2>
+      <h2 class="uk-text-light uk-text-muted uk-text-uppercase">{$_("wallet.account_details.title")}</h2>
     </div>
   </div>
 
@@ -64,7 +26,7 @@
           <div class="uk-width-1-2@m">
             <AccountNote {signingAccount} />
           </div>
-          {#if $signingAccount.on_chain && !$signingAccount.watch_only}
+          {#if !$signingAccount.watch_only}
             <div class="uk-width-1-2@m">
               <GetPrivateKey />
             </div>
@@ -74,59 +36,17 @@
           </div>          
         </div>
         <div class="uk-margin-top uk-flex uk-flex-left uk-flex-wrap">
-          <button 
-            class="uk-button uk-button-danger uk-margin-small-right" 
-            on:click|preventDefault={handleRemoveAccount}
-            >Remove Account</button>
+          <RemoveAccount {signingAccount} />
         </div>
       </div>
-    </section>  
-
-  {/if}
-
-   <!-- REMOVE ACCOUNT MODAL -->
-   <div id="removeAccountModal" uk-modal class="uk-modal-container">
-    <div class="uk-modal-dialog uk-modal-body uk-padding-large">
-      <div>
-        <h2 class="uk-modal-title uk-text-uppercase uk-text-danger">
-          Remove Account - {$signingAccount && $signingAccount.nickname}
-        </h2>
-        <h3 class="uk-text-bold">Are you sure you want to remove this account?</h3>
-        <p class="uk-text-lead uk-text-warning">Please review the following before proceeding:</p>
-        <ul class="uk-list uk-list-bullet uk-text-left uk-margin-top uk-margin-large-bottom">
-          <li>After removing your account from this wallet, you won't be able to recover its private key.</li>
-          <li>Ensure you have a <strong>backup of your private key</strong> before proceeding.</li>
-          <li>This action will only remove the account from this wallet, not from the blockchain.</li>
-        </ul>
-      </div>        
-      <div class="uk-margin-top uk-text-right">
-        <button 
-          class="uk-button uk-button-default uk-margin-right uk-modal-close"
-          disabled={!isRemoving}
-        >
-          Cancel
-        </button>
-        <button 
-          id="confirmRemoveAccountBtn"
-          type="button"
-          class="uk-button uk-button-danger" 
-          disabled={!isRemoving}
-          on:click|preventDefault={handleConfirmRemoveAccount}
-        >
-          Confirm Remove Account
-        </button>
-      </div>
-    </div>
-  </div>
+    </section>
+  {/if}  
 </main>
 
 <style>
   .uk-container {
     max-width: 800px;
     margin: auto;
-  }
-  button {
-    margin-right: 10px;
   }
   section {
     margin-top: 20px;
